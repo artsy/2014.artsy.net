a949ae34281fe4c85745da973ccd2076051e5005
dzucconi <mail@damonzucconi.com>
2015-01-09 17:52:34 -0500

    Extract out copy into a JSON file

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,4 +3,5 @@ var gulp = require('gulp');
 gulp.task('watch', ['set:watch', 'sync'], function() {
   gulp.watch('src/assets/stylesheets/**', ['build:css']);
   gulp.watch('src/html/**', ['template:development']);
+  gulp.watch('src/data/**', ['template:development']);
 });
