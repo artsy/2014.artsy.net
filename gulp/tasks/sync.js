5d46fcb887637201c124a4c07bc1c29a8e5731f1
dzucconi <mail@damonzucconi.com>
2015-01-11 22:13:49 -0500

    Disable synced scrolling

--- gulp/tasks/sync.js
+++ gulp/tasks/sync.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 
 gulp.task('sync', ['template:development', 'build:js', 'build:css', 'build:images'], function() {
   sync.init(['build/**'], {
+    ghostMode: {
+      scroll: false
+    },
     server: {
       baseDir: 'build'
     }
