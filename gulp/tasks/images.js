2f3afd0b50becdbf9e4eb25d53f635c99c89c4c2
dzucconi <mail@damonzucconi.com>
2015-01-14 11:05:19 -0500

    Adds preliminary task for resizing images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,6 +1,48 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var imageResize = require('gulp-image-resize');
 
 gulp.task('build:images', ['clean:images'], function() {
-  return gulp.src('src/assets/images/*')
+  return gulp.src('src/assets/images/**/*')
     .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('resize:images', function() {
+  var src = 'src/assets/images/originals/*.jpg';
+  var quality = 0.95;
+
+  gulp.src(src).pipe(imageResize({
+      width: 600,
+      height: 350,
+      quality: quality,
+      crop: true,
+      upscale: true
+    }))
+    .pipe(gulp.dest('build/images/600x350'));
+
+  gulp.src(src).pipe(imageResize({
+      width: 960,
+      height: 960,
+      quality: quality,
+      crop: false,
+      upscale: true
+    }))
+    .pipe(gulp.dest('build/images/960'));
+
+  gulp.src(src).pipe(imageResize({
+      width: 1920,
+      height: 1920,
+      quality: quality,
+      crop: false,
+      upscale: true
+    }))
+    .pipe(gulp.dest('build/images/1920'));
+
+  gulp.src(src).pipe(imageResize({
+      width: 350,
+      quality: quality,
+      crop: false,
+      upscale: true
+    }))
+    .pipe(gulp.dest('build/images/350w'));
 });
