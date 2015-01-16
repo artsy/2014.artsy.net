f2c800ab229dd56e1470c0ced696590695d969f3
dzucconi <mail@damonzucconi.com>
2015-01-16 15:43:29 -0500

    More precise image handling

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -10,13 +10,16 @@ gulp.task('build:images:production', function(done) {
 });
 
 gulp.task('move:images', function() {
-  gulp.src('src/assets/images/**/*')
+  gulp.src([
+    'src/assets/images/*.*',
+    'src/assets/images/resized/**/*',
+  ])
     .pipe(gulp.dest('build/images'));
 });
 
-var resize = function(name, options) {
-  var destination = 'src/assets/images/resized/' + name;
-  gulp.src('src/assets/images/originals/*.jpg')
+var resize = function(source, version, options) {
+  var destination = 'src/assets/images/resized/' + source + '/' + version;
+  gulp.src('src/assets/images/originals/' + source + '/' + '*.jpg')
     .pipe(changed(destination))
     .pipe(imageResize(options))
     .pipe(imagemin({ progressive: true }))
@@ -24,9 +27,47 @@ var resize = function(name, options) {
 };
 
 gulp.task('resize:images', function() {
-  var quality = 0.9;
-  resize('600x350', { width: 600, height: 350, quality: quality, crop: true, upscale: true });
-  resize('960', { width: 960, height: 960, quality: quality, crop: true, upscale: false });
-  resize('1920', { width: 1920, height: 1920, quality: quality, crop: false, upscale: false });
-  resize('350w', { width: 350, quality: quality, crop: false, upscale: false });
+  var quality = {
+    "veryHigh": 0.9,
+    "high": 0.8
+  };
+
+  // Web:
+
+  // Backdrops
+  resize('web/backdrops', '1x', {
+    width: 1920, height: 1080, quality: quality.high, crop: false, upscale: false
+  });
+
+  // Crops
+  resize('web/crops', '1x', {
+    width: 500, height: 300, quality: quality.veryHigh, crop: true, upscale: true
+  });
+  resize('web/crops', '2x', {
+    width: 1000, height: 600, quality: quality.high, crop: true, upscale: true
+  });
+
+  // Singles
+  resize('web/singles', '1x', {
+    width: 800, height: 800, quality: quality.veryHigh, crop: false, upscale: false
+  });
+  resize('web/singles', '2x', {
+    width: 1600, height: 1600, quality: quality.high, crop: false, upscale: false
+  });
+
+  // Columns
+  resize('web/columns', '1x', {
+    width: 350, quality: quality.veryHigh, crop: false, upscale: true
+  });
+  resize('web/columns', '2x', {
+    width: 700, quality: quality.high, crop: false, upscale: true
+  });
+
+  // Categories
+  resize('web/categories', '1x', {
+    height: 200, quality: quality.veryHigh, crop: false, upscale: true
+  });
+  resize('web/categories', '2x', {
+    height: 400, quality: quality.high, crop: false, upscale: true
+  });
 });
