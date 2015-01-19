dfd167a24123528df26366554f3561535e6499c3
dzucconi <mail@damonzucconi.com>
2015-01-19 16:11:30 -0500

    Remove gulp-cloudfront

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var awspublish = require('gulp-awspublish');
-var cloudfront = require("gulp-cloudfront");
 
 var aws = {
   key: process.env.AWS_KEY,
   secret: process.env.AWS_SECRET,
-  bucket: process.env.S3_BUCKET,
-  distributionId: process.env.DISTRIBUTION_ID,
-  patternIndex: /^index\.html$/
+  bucket: process.env.S3_BUCKET
 };
 
 gulp.task('deploy', ['compress'], function() {
@@ -19,5 +16,4 @@ gulp.task('deploy', ['compress'], function() {
     .pipe(publisher.sync())
     .pipe(publisher.cache())
     .pipe(awspublish.reporter());
-    // .pipe(cloudfront(aws));
 });
