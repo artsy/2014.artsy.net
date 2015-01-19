8a2efb8b5a5018ce383362248d092fb47e9872c3
dzucconi <mail@damonzucconi.com>
2015-01-18 20:41:14 -0500

    Invalidate cloudfront cache on deploy

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var awspublish = require('gulp-awspublish');
+var cloudfront = require("gulp-cloudfront");
+
+var aws = {
+  key: process.env.AWS_KEY,
+  secret: process.env.AWS_SECRET,
+  bucket: process.env.S3_BUCKET
+  distributionId: process.env.DISTRIBUTION_ID
+};
 
 gulp.task('deploy', ['compress'], function() {
-  var publisher = awspublish.create({
-    key: process.env.AWS_KEY,
-    secret: process.env.AWS_SECRET,
-    bucket: process.env.S3_BUCKET
-  });
+  var publisher = awspublish.create(aws);
 
   return gulp.src('./build/**/*')
     .pipe(publisher.publish())
     .pipe(publisher.sync())
     .pipe(publisher.cache())
-    .pipe(awspublish.reporter());
+    .pipe(awspublish.reporter())
+    .pipe(cloudfront(aws));
 });
