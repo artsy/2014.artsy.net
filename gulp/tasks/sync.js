a406c19ab13652915c81932c54e8f4087532e984
dzucconi <mail@damonzucconi.com>
2015-02-24 13:26:48 -0500

    Removes browser-sync

--- gulp/tasks/sync.js
+++ gulp/tasks/sync.js
@@ -1,13 +1,5 @@
-var sync = require('browser-sync');
 var gulp = require('gulp');
 
-gulp.task('sync', ['template:development', 'build:js', 'build:css', 'move:images', 'move:videos'], function() {
-  sync.init(['build/**'], {
-    ghostMode: {
-      scroll: false
-    },
-    server: {
-      baseDir: 'build'
-    }
-  });
+gulp.task('sync', ['template:development', 'build:js', 'build:css', 'move:images', 'move:videos'], function(done) {
+  done()
 });
